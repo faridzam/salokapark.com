ad7ce611a28256a2dcd27d59015678f23eb2f330
faridzam <skinel123@gmail.com>
2022-11-11 09:43:41 +0000

    gtm

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,20 +9,12 @@ import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import { Inertia } from '@inertiajs/inertia';
-
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
-
-        Inertia.on('navigate', (event) => {
-            gtag('event', 'page_view', {
-             'page_location': event.detail.page.url
-            });
-        })
 
         const root = createRoot(el);
         root.render(
