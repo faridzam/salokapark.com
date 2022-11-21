1813d7e9ff1129e421a130d76fefc5314afd36d7
faridzam <skinel123@gmail.com>
2022-11-21 04:13:49 +0000

    add compression to vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
     plugins: [
@@ -12,6 +13,7 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        viteCompression(),
     ],
     ssr: {
         noExternal: ['@inertiajs/server'],
